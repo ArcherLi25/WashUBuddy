3c078859fece6121d1ec5d4d4fb4cfce2c1ce3e5
Archer Li <102427092+ArcherLi25@users.noreply.github.com>
2025-01-20 14:58:01 -0600

    model nb, added VecStore, new requirements.txt

---
+++ src/chatbot.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from streamlit_chat import message
+from src.qa import ask_and_get_answer
+
+def chatbot():
+    if 'history' not in st.session_state:
+        st.session_state.history = []
+
+    q = st.text_input('WASHU BUDDY ADMIN PANEL', placeholder='Ask me anything about Washington University in St. Louis...')
+    if q:
+        if 'vs' in st.session_state:
+            vector_store = st.session_state.vs
+            answer = ask_and_get_answer(vector_store, q)
+            
+            st.session_state.history.append({"text": q, "is_user": True})
+            st.session_state.history.append({"text": answer, "is_user": False})
+        else:
+            st.write("Vector store not found in session state.")
+
+    for chat in st.session_state.history:
+        message(chat['text'], is_user=chat['is_user'])
--- src/main.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv, find_dotenv
-import streamlit as st
-from src.app import run_app
-
-if __name__ == "__main__":
-    load_dotenv(find_dotenv(), override=True)
-    run_app()
